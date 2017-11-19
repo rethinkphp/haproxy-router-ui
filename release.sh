ca3358d0abf8a2e9a7a9652061c0f91f3f3420ce
Jin Hu <bixuehujin@gmail.com>
2017-11-19 14:50:10 +0800

    Added release.sh to release tarbar

---
+++ release.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+if [[ $1 == '' ]] ; then
+    echo 'release version is required'
+    exit 1
+fi
+
+NODE_ENV=production npm run build || exit 1
+
+cd dist
+
+tar cf ui-$1.tar.gz index.html main.*
